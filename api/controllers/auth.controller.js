728ce9b00b6ab12c24adcefd50bf09a6fb7806b9
martins091 <agbomartinsejiofor@gmail.com>
2024-01-17 17:32:04 +0100

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,18 +1,37 @@
-import User from '../models/user.model.js'
-import bcryptjs from 'bcryptjs';
-import { errorHandler } from '../utils/error.js';
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
-export  const signup = async (req, res, next) => {
-    const { username, email, password } = req.body;
-    const hashedPassword = bcryptjs.hashSync(password, 10);
-    const newUser = new User({ username, email, password: hashedPassword });
-    
-    try {
-        await  newUser.save()
-        res.status(201).json({ message: "user created successfully" });
-        console.log(hashedPassword)
-    } catch (error) {
-        next(errorHandler(300, "something went wrong"));
-    }
+export const signup = async (req, res, next) => {
+  const { username, email, password } = req.body;
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
 
-} 
+  try {
+    await newUser.save();
+    res.status(201).json({ message: "user created successfully" });
+    console.log(hashedPassword);
+  } catch (error) {
+    next(errorHandler(300, "something went wrong"));
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(401, "User not found"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401), "wrong credentials");
+    const token = jwt.sign({ _id: validUser._id }, process.env.JWT_SECRET);
+    const { password: hashedPassword, ...rest } = validUser._doc;
+    const expiryDate = new Date(Date.now() + 3600000); // 1 hour
+    res
+      .cookie("access_token", token, { httpOnly: true, expires: expiryDate })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
