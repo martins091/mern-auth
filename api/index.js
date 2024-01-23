de04d2c897fbfe4b1a425ad75ab66fae5b394cff
martins091 <agbomartinsejiofor@gmail.com>
2024-01-23 13:53:31 +0100

    add pre production changes

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv'
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js'
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 
 dotenv.config();
@@ -14,9 +15,17 @@ mongoose.connect(process.env.MONGO)
 })
 .catch((err) => {
     console.log(err);
-})
+});
+
+const __dirname = path.resolve()
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, 'client/dist')))
+
+app.get('[*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+});
 
 app.use(express.json());
 
